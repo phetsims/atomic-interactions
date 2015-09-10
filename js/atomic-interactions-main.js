ef0c9603aecc2467e83ac2f5abe4675b82c2df7f
Chris Malley <cmalley@pixelzoom.com>
2015-09-10 17:52:50 -0600

    change repo.name convention to repo.title phetsims/chipper#340

--- js/atomic-interactions-main.js
+++ js/atomic-interactions-main.js
@@ -16,7 +16,7 @@ define( function( require ) {
   var Property = require( 'AXON/Property' );
 
   // strings
-  var simTitle = require( 'string!ATOMIC_INTERACTIONS/atomic-interactions.name' );
+  var simTitle = require( 'string!ATOMIC_INTERACTIONS/atomic-interactions.title' );
 
   var projectorModeProperty = new Property( false );
   var simOptions = {
