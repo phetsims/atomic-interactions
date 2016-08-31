08bff1e64781014b694b2f25ecdf7d7ce0b7916f
Chris Malley <cmalley@pixelzoom.com>
2016-08-31 14:46:57 -0600

    delete no-op simOptions extend from main.js, https://github.com/phetsims/simula-rasa/issues/8

--- js/atomic-interactions-main.js
+++ js/atomic-interactions-main.js
@@ -33,13 +33,6 @@ define( function( require ) {
     optionsNode: new GlobalOptionsNode( projectorModeProperty )
   };
 
-  // Appending '?dev' to the URL will enable developer-only features.
-  if ( phet.chipper.getQueryParameter( 'dev' ) ) {
-    simOptions = _.extend( {
-      // add dev-specific options here
-    }, simOptions );
-  }
-
   SimLauncher.launch( function() {
     var sim = new Sim(
       atomicInteractionsTitleString,
