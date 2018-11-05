11bca019e7d52ffe55c12b9290637892b769be58
jbphet <john.blanco@colorado.edu>
2018-11-05 12:39:09 -0700

    updated credits for all sims in the States of Matter suite

--- js/atomic-interactions-main.js
+++ js/atomic-interactions-main.js
@@ -19,12 +19,12 @@ define( function( require ) {
 
   var simOptions = {
     credits: {
-      leadDesign: 'Paul Beale, Yuen-ying Carpenter, Sarah McKagan, Emily Moore, Noah Podolefsky, Amy Rouinfar',
-      softwareDevelopment: 'John Blanco, Aadish Gupta',
-      team: 'Wendy Adams, Jack Barbera, Amy Hanson, Kelly Lancaster, Ariel Paul, Kathy Perkins, Carl Weiman',
-      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Oliver Orejola, Benjamin Roberts, Bryan Yoelin',
-      thanks: 'Thanks to Actual Concepts for working with the PhET development team to convert this simulation to ' +
-              'HTML5.'
+      leadDesign: 'Paul Beale, Yuen-ying Carpenter, Sarah McKagan, Emily Moore, Noah Podolefsky,<br>Amy Rouinfar',
+      softwareDevelopment: 'John Blanco, Aaron Davis, Aadish Gupta',
+      team: 'Wendy Adams, Jack Barbera, Amy Hanson, Kelly Lancaster, Ariel Paul, Kathy Perkins,<br>Carl Wieman',
+      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson, Liam Mulhall,<br>' +
+                        'Oliver Orejola, Laura Rea, Benjamin Roberts, Jacob Romero, Kathryn Woessner, Bryan Yoelin',
+      thanks: 'Thanks to Actual Concepts for working with the PhET development team to convert this simulation to HTML5.'
     },
     optionsNode: new GlobalOptionsNode()
   };
