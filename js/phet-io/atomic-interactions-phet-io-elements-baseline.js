2b6466d058c84a44362b11203666bdb744215876
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:58:01 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/atomic-interactions-phet-io-elements-baseline.js
+++ js/phet-io/atomic-interactions-phet-io-elements-baseline.js
@@ -4825,7 +4825,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "NumberPropertyIO"
     },
     "atomicInteractions.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
