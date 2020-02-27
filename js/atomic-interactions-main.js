dd66c641ed970cceab6b8fe856ff767adcae8e15
samreid <reids@colorado.edu>
2020-02-27 11:38:05 -0700

    Add repo to migration list, see https://github.com/phetsims/chipper/issues/875

--- js/atomic-interactions-main.js
+++ js/atomic-interactions-main.js
@@ -5,41 +5,37 @@
  *
  * @author John Blanco
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const AtomicInteractionsScreen = require( 'STATES_OF_MATTER/atomic-interactions/AtomicInteractionsScreen' );
-  const GlobalOptionsNode = require( 'STATES_OF_MATTER/common/view/GlobalOptionsNode' );
-  const Sim = require( 'JOIST/Sim' );
-  const SimLauncher = require( 'JOIST/SimLauncher' );
-  const Tandem = require( 'TANDEM/Tandem' );
+import Sim from '../../joist/js/Sim.js';
+import SimLauncher from '../../joist/js/SimLauncher.js';
+import AtomicInteractionsScreen from '../../states-of-matter/js/atomic-interactions/AtomicInteractionsScreen.js';
+import GlobalOptionsNode from '../../states-of-matter/js/common/view/GlobalOptionsNode.js';
+import Tandem from '../../tandem/js/Tandem.js';
+import atomicInteractionsStrings from './atomic-interactions-strings.js';
 
-  // strings
-  const atomicInteractionsTitleString = require( 'string!ATOMIC_INTERACTIONS/atomic-interactions.title' );
+const atomicInteractionsTitleString = atomicInteractionsStrings[ 'atomic-interactions' ].title;
 
-  // Eagerly create GlobalOptionsNode so it works smoothly with PhET-iO
-  const globalOptionsNode = new GlobalOptionsNode( Tandem.ROOT.createTandem( 'global' ).createTandem( 'view' ).createTandem( 'globalOptionsNode' ) );
+// Eagerly create GlobalOptionsNode so it works smoothly with PhET-iO
+const globalOptionsNode = new GlobalOptionsNode( Tandem.ROOT.createTandem( 'global' ).createTandem( 'view' ).createTandem( 'globalOptionsNode' ) );
 
-  const simOptions = {
-    credits: {
-      leadDesign: 'Paul Beale, Yuen-ying Carpenter, Sarah McKagan, Emily B. Moore, Noah Podolefsky,<br>Amy Rouinfar',
-      softwareDevelopment: 'John Blanco, Aaron Davis, Aadish Gupta',
-      team: 'Wendy Adams, Jack Barbera, Amy Hanson, Kelly Lancaster, Ariel Paul, Kathy Perkins,<br>Carl Wieman',
-      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson, Liam Mulhall,<br>' +
-                        'Oliver Orejola, Laura Rea, Benjamin Roberts, Jacob Romero, Kathryn Woessner, Bryan Yoelin',
-      thanks: 'Thanks to Actual Concepts for working with the PhET development team to convert this simulation to HTML5.'
-    },
+const simOptions = {
+  credits: {
+    leadDesign: 'Paul Beale, Yuen-ying Carpenter, Sarah McKagan, Emily B. Moore, Noah Podolefsky,<br>Amy Rouinfar',
+    softwareDevelopment: 'John Blanco, Aaron Davis, Aadish Gupta',
+    team: 'Wendy Adams, Jack Barbera, Amy Hanson, Kelly Lancaster, Ariel Paul, Kathy Perkins,<br>Carl Wieman',
+    qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson, Liam Mulhall,<br>' +
+                      'Oliver Orejola, Laura Rea, Benjamin Roberts, Jacob Romero, Kathryn Woessner, Bryan Yoelin',
+    thanks: 'Thanks to Actual Concepts for working with the PhET development team to convert this simulation to HTML5.'
+  },
 
-    // create content for the Options dialog
-    createOptionsDialogContent: () => globalOptionsNode
-  };
+  // create content for the Options dialog
+  createOptionsDialogContent: () => globalOptionsNode
+};
 
-  SimLauncher.launch( function() {
-    const sim = new Sim(
-      atomicInteractionsTitleString,
-      [ new AtomicInteractionsScreen( true, atomicInteractionsTitleString, Tandem.ROOT.createTandem( 'atomicInteractionsScreen' ) ) ],
-      simOptions );
-    sim.start();
-  } );
+SimLauncher.launch( function() {
+  const sim = new Sim(
+    atomicInteractionsTitleString,
+    [ new AtomicInteractionsScreen( true, atomicInteractionsTitleString, Tandem.ROOT.createTandem( 'atomicInteractionsScreen' ) ) ],
+    simOptions );
+  sim.start();
 } );
---
+++ js/atomic-interactions-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import atomicInteractions from './atomicInteractions.js';
+
+const atomicInteractionsStrings = getStringModule( 'ATOMIC_INTERACTIONS' );
+
+atomicInteractions.register( 'atomicInteractionsStrings', atomicInteractionsStrings );
+
+export default atomicInteractionsStrings;
