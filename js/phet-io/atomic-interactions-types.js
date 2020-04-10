fba215fa42d44e4f6386bee555994e03e3590fb7
pixelzoom <cmalley@pixelzoom.com>
2020-04-10 15:51:49 -0600

    run 'grunt update' for API files that were incorrect in master, https://github.com/phetsims/phet-io/issues/1623

--- js/phet-io/atomic-interactions-types.js
+++ js/phet-io/atomic-interactions-types.js
@@ -219,6 +219,9 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
+      "parameterTypes": [
+        "StringIO"
+      ],
       "supertype": "ObjectIO",
       "typeName": "ArrayIO<StringIO>"
     },
@@ -1657,6 +1660,9 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
+      "parameterTypes": [
+        "ObjectIO"
+      ],
       "supertype": "ObjectIO",
       "typeName": "ReferenceIO<ObjectIO>"
     },
@@ -1681,6 +1687,9 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
+      "parameterTypes": [
+        "ObjectIO"
+      ],
       "supertype": "ReferenceIO<ObjectIO>",
       "typeName": "ScreenIO"
     },
