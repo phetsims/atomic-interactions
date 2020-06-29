020db6100b1b330689015982a7a5cefc9ad3fe13
samreid <reids@colorado.edu>
2020-06-29 14:38:16 -0600

    Move radiusProperty.phetioFeatured from overrides to main code, see https://github.com/phetsims/states-of-matter/issues/303

--- js/phet-io/atomic-interactions-overrides.js
+++ js/phet-io/atomic-interactions-overrides.js
@@ -1,9 +1,6 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
 {
-  "atomicInteractions.atomicInteractionsScreen.model.fixedAtom.radiusProperty": {
-    "phetioFeatured": true
-  },
   "atomicInteractions.atomicInteractionsScreen.model.forcesDisplayModeProperty": {
     "phetioFeatured": true
   },
