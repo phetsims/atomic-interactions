8c25960bb3f1eb7f7f4a8ec97c75e4489ca4ec39
samreid <reids@colorado.edu>
2020-07-03 11:02:32 -0600

    Revert "Move phetioFeatured from overrides to main code, see https://github.com/phetsims/states-of-matter/issues/303"
    This reverts commit abe94f6c

--- js/phet-io/atomic-interactions-overrides.js
+++ js/phet-io/atomic-interactions-overrides.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
 {
+  "atomicInteractions.atomicInteractionsScreen.model.isPlayingProperty": {
+    "phetioFeatured": true
+  },
   "atomicInteractions.atomicInteractionsScreen.model.movableAtom.accelerationProperty": {
     "phetioFeatured": true
   },
