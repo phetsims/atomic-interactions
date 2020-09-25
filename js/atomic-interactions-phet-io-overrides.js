65691a0d464fcaceff49de27b89e6a32399d3fed
samreid <reids@colorado.edu>
2020-09-25 16:15:20 -0600

    Reformat overrides, see https://github.com/phetsims/studio/issues/196

--- js/atomic-interactions-phet-io-overrides.js
+++ js/atomic-interactions-phet-io-overrides.js
@@ -1,85 +1,85 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
-{
-  "atomicInteractions.atomicInteractionsScreen.model.adjustableAtomDiameterProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.atomPairProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.fixedAtom.radiusProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.forcesDisplayModeProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.forcesExpandedProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.adjustableAtomInteractionStrengthProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.isPlayingProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.movableAtom.accelerationProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.movableAtom.positionProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.movableAtom.radiusProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.movableAtom.velocityProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.movementHintVisibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.model.timeSpeedProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.atomDiameterSlider.title.textProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.atomDiameterSlider.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.interactionStrengthSlider.title.textProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.interactionStrengthSlider.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.epsilonGraphLabel.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.interactiveControls.epsilon.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.interactiveControls.sigma.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.interactiveControls.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.positionMarker.pickableProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.sigmaGraphLabel.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.movableParticleNode.pickableProperty": {
-    "phetioFeatured": true
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.returnAtomButton.enabledProperty": {
-    "phetioFeatured": false
-  },
-  "atomicInteractions.atomicInteractionsScreen.view.returnAtomButton.visibleProperty": {
-    "phetioFeatured": false
-  }
-};
+  {
+    "atomicInteractions.atomicInteractionsScreen.model.adjustableAtomDiameterProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.atomPairProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.fixedAtom.radiusProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.forcesDisplayModeProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.forcesExpandedProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.adjustableAtomInteractionStrengthProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.isPlayingProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.movableAtom.accelerationProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.movableAtom.positionProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.movableAtom.radiusProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.movableAtom.velocityProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.movementHintVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.model.timeSpeedProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.atomDiameterSlider.title.textProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.atomDiameterSlider.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.interactionStrengthSlider.title.textProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.interactionStrengthSlider.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.atomsControlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.epsilonGraphLabel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.interactiveControls.epsilon.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.interactiveControls.sigma.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.interactiveControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.positionMarker.pickableProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.interactivePotentialGraph.sigmaGraphLabel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.movableParticleNode.pickableProperty": {
+      "phetioFeatured": true
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.returnAtomButton.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "atomicInteractions.atomicInteractionsScreen.view.returnAtomButton.visibleProperty": {
+      "phetioFeatured": false
+    }
+  };
