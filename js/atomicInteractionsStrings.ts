3a28558d8a94558c338ecbed9079a8700179e95c
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:39 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/atomicInteractionsStrings.ts
+++ js/atomicInteractionsStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import atomicInteractions from './atomicInteractions.js';
 
-const atomicInteractionsStrings = getStringModule( 'ATOMIC_INTERACTIONS' );
+type StringsType = {
+  'atomic-interactions': {
+    'title': string
+  }
+};
+
+const atomicInteractionsStrings = getStringModule( 'ATOMIC_INTERACTIONS' ) as StringsType;
 
 atomicInteractions.register( 'atomicInteractionsStrings', atomicInteractionsStrings );
 
