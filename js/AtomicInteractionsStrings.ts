3fed88b34f4351b418586b29244d7e0e9cf71567
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-20 12:16:27 -0600

    remove as many LinkableElement usages as possible, using PhetioProperty where we have to, https://github.com/phetsims/tandem/issues/299
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/AtomicInteractionsStrings.ts
+++ js/AtomicInteractionsStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import LinkableProperty from '../../axon/js/LinkableProperty.js';
+import type LocalizedStringProperty from '../../chipper/js/LocalizedStringProperty.js';
 import atomicInteractions from './atomicInteractions.js';
 
 type StringsType = {
   'atomic-interactions': {
     'title': string;
-    'titleStringProperty': LinkableProperty<string>;
+    'titleStringProperty': LocalizedStringProperty;
   }
 };
 
