c4b2d026b89d773fcd01f82594326881157862cb
Sam Reid <reids@colorado.edu>
2026-03-19 15:56:24 -0600

    Remove register calls and optimize imports, see https://github.com/phetsims/phet-core/issues/100

--- js/AtomicInteractionsStrings.ts
+++ js/AtomicInteractionsStrings.ts
@@ -20,6 +20,4 @@ type StringsType = {
 
 const AtomicInteractionsStrings = getStringModule( 'ATOMIC_INTERACTIONS' ) as StringsType;
 
-atomicInteractions.register( 'AtomicInteractionsStrings', AtomicInteractionsStrings );
-
 export default AtomicInteractionsStrings;
